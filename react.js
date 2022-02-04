7624e5c491ff4bebfa0f9e759ca7d2e437275d0a
ankitadwivedi1 <60252894+ankitadwivedi1@users.noreply.github.com>
2022-02-04 19:14:01 +0530

    Create react.js

---
+++ react.js
@@ -0,0 +1,19 @@
+import React from 'react';
+export function Search(){
+  return( 
+    <div>
+     <div className="search-input">
+    <input type="text" placeholder="Search"/>
+    </div>
+    
+     <div>
+     <div className="search-input">
+    <input type="Email" placeholder="Search"/>
+    </div>
+    
+     <div>
+     <div className="search-input">
+    <input type="password" placeholder="Search"/>
+    </div>
+    
+    
